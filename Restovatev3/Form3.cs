a97394e99f4fb6cce56ce8c35289cdf2264a8988
Raphael Z. Briones <raphaelzbriones@icloud.com>
2017-11-04 14:46:39 +0800

    Update 1.0: Uploading the first part of the application
    This includes: Forms 1,2,3 with SQL Database Implementation.

---
+++ Restovatev3/Form3.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restovatev3
+{
+    public partial class Form3 : Form
+    {
+        int itemid;
+ 
+        public Form3(int itemid)
+        {
+            this.itemid = itemid;
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            SqlDataReader rdr = null;
+            string constring;
+            constring = "Data Source=(local);Initial Catalog=RestovateDatabase;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constring);
+            string cmdText = "Select * from ItemDescript where ItemID = " + itemid;
+            SqlCommand cmd = new SqlCommand(cmdText,con);
+
+            try
+            {
+                con.Open();
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    string prodname = (string)rdr["ProdName"];
+                    string prodtype = (string)rdr["ProdType"];
+                    string proddescript = (string)rdr["ProdDescript"];
+
+
+                    this.Text = prodname;
+                    this.label1.Text = prodname;
+                    this.label2.Text = prodtype;
+                    this.label3.Text = proddescript;
+                }
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+    }
+}
